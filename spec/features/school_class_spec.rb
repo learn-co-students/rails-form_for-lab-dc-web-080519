b5e17a51e8e5d730b4594790e74802793a9c77f9
Pete Hanner <petehanner@gmail.com>
2019-09-02 20:05:23 -0400

    combining MVC with form_for

--- spec/features/school_class_spec.rb
+++ spec/features/school_class_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 
-describe 'form page' do
+describe 'School Class form page: ' do
   it 'form renders with the new action' do
     visit new_school_class_path
     expect(page).to have_content("School Class Form")
@@ -56,7 +56,7 @@ describe 'form page' do
   end
 end
 
-describe 'Show page' do
+describe 'School Class show page:' do
   let(:school_class) { SchoolClass.create(title: "Computer Science", room_number: 5) }
 
   it 'renders properly' do
